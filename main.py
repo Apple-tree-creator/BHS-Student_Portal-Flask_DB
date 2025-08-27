52b623213c4d4fa0b9470c6e382d58204f30d4f2
Chris <christopherfung888@gmail.com>
2025-08-28 11:30:27 +1200

    Database functionality

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
 
 vars = {
-       'site_title': "Untitled Site",
+       'site_title': "BHS Portal",
        'anim_speed': '300ms', # You must add unit
        }
 
 # basic route
 @app.route('/')
 def root():
-         title = 'Home'
-         return render_template('home.html', vars=vars, title=title)
+         conn = sqlite3.connect('sites.db')
+         cur = conn.cursor()
+         cur.execute('SELECT Name, URL FROM Pinned ORDER BY Name ASC;')
+
+         sites = cur.fetchall()
+         print(sites)
+         conn.close()
+         return render_template('directories/home.html', vars=vars, title='Home', sites=sites)
+
+@app.route('/school-sites')
+def sites():
+         conn = sqlite3.connect('sites.db')
+         cur = conn.cursor()
+         cur.execute('SELECT Name, URL FROM School_sites ORDER BY Name ASC;')
+
+         sites = cur.fetchall()
+         print(sites)
+         conn.close()
+         return render_template('directories/school-sites.html', vars=vars, title='School Sites', sites=sites)
+
+@app.route('/forums')
+def forums():  
+         conn = sqlite3.connect('sites.db')
+         cur = conn.cursor()
+         cur.execute('SELECT Name, URL FROM Forums ORDER BY Name ASC;')
+
+         sites = cur.fetchall()
+         print(sites)
+         conn.close() 
+         return render_template('directories/forums.html', vars=vars, title='Forums', sites=sites)
+
+@app.route('/tools')
+def tools(): 
+         conn = sqlite3.connect('sites.db')
+         cur = conn.cursor()
+         cur.execute('SELECT Name, URL FROM Tools ORDER BY Name ASC;')
+
+         sites = cur.fetchall()
+         print(sites)
+         conn.close()  
+         return render_template('directories/tools.html', vars=vars, title='Tools', sites=sites)
 
 if __name__ == "__main__":
     app.run(debug=True)
